bea6e5fd9dbabbfbac2192cd4195ecd74a38146b
EduardoMatosBH <eduardopde@gmail.com>
2017-10-18 22:26:19 -0200

    ADD

---
+++ Trabalho/src/trabalho/ContaCorrente.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho;
+
+/**
+ *
+ * @author 71501118
+ */
+public class ContaCorrente extends Conta {
+    
+    private double chequeEspecial;
+    private double anuidade;
+    
+     public void saque(){
+    }
+    public void saldo(){
+    }
+    public void extrato(){
+    }
+    public void deposito(){
+    }
+    
+}
---
+++ Trabalho/src/trabalho/ContaPoupanca.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho;
+
+/**
+ *
+ * @author 71501118
+ */
+public class ContaPoupanca extends Conta{
+    
+    private double rendimento;
+    
+     public void saque(){
+    }
+    public void saldo(){
+    }
+    public void extrato(){
+    }
+    public void deposito(){
+    }
+}
